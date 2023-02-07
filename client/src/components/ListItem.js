9b35dec555aecf950fb6a4c241675268e02f415d
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-02-07 22:03:19 +0300

    button stilleri hazırlandı

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,8 +1,22 @@
+import TickIcon from "./TickIcon";
+import ProgressBar from "./ProgressBar";
+
+
 const ListItem =({task})=> {
     return (
-      <div >
-        <p>{task.title}</p>
-      </div>
+      <li className="list-item">
+        <div className="info-container">
+          <TickIcon/>
+          <p className="task-title">{task.title}</p>
+          <ProgressBar/>
+        </div>
+
+        <div className="button-container">
+          <button className="edit">EDIT</button>
+          <button className="delete">DELETE</button>
+        </div>
+        
+      </li>
     );
   }
   
